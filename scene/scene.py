4451a5187100ec42fdb75a0a08dec9feb55ac90c
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-25 20:04:44 +0530

    Update scene.py

--- scene/scene.py
+++ scene/scene.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import imageio
 
-from Nirvana.camera import *
+from Nirvana.camera.camera import *
 
 class Scene:
     def __init__(self):
