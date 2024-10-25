75eb6df686ecc2ca107c76d3049c5b70479e6f4f
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-25 19:47:33 +0530

    Added functionality to convert everything to 0-1 float RGB
    Converts RGB values to float RGB.

--- lights/light.py
+++ lights/light.py
@@ -3,11 +3,14 @@ import numpy as np
 class LightSource:
     def __init__(self, location=(0, 0, 0), orientation=(0, 0, 0), intensity=0.5, color=(1, 1, 1), epsilon=0.001):
         self.location = np.array(location)
-        self.color = np.array(color)
-        self.intensity = intensity
-        self.color = np.array(color)
+        
+        # Normalize color to the range [0, 1] if it's in [0, 255]
+        self.color = np.array(color, dtype=float)
+        if np.any(self.color > 1.0):  # Check if any value is greater than 1, assuming [0, 255] range
+            self.color = self.color / 255.0  # Normalize to [0, 1]
 
+        self.intensity = intensity
+        
         # Calculate norm of the orientation vector
         norm = np.linalg.norm(orientation)
-
-        self.orientation = np.array(orientation)/(norm + epsilon) * intensity
+        self.orientation = np.array(orientation) / (norm + epsilon) * intensity
