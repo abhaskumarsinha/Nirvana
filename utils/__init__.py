71ac9ce1c2cebdcdc0075f383d54cd4120334403
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-28 19:37:37 +0530

    Added support for fresnel formula

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 from Nirvana.utils.lambert_reflection import *
 from Nirvana.utils.GGX_Distribution import *
 from Nirvana.utils.GGX_Geometry import *
+from Nirvana.utils.fresnel_schlick import *
