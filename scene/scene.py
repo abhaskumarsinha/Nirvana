2a269de3293821214ee5137dd24162040f134ba5
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-28 13:52:12 +0530

    Debugging error here

--- scene/scene.py
+++ scene/scene.py
@@ -263,7 +263,7 @@ class Scene:
 
                 # Now clip the values to [0, 1] and plot
                 face_color = np.clip(face_color, 0, 1)
-
+                print('Face Color: ', face_color)
                 polygon = patches.Polygon(face, closed=True, facecolor=(face_color, face_color, face_color), alpha=1)
                 ax.add_patch(polygon)                
 
