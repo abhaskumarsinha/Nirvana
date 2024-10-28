765470df76dba388cce516e9de40eed2826a0e09
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-28 19:20:43 +0530

    Checking face color value

--- scene/scene.py
+++ scene/scene.py
@@ -277,7 +277,9 @@ class Scene:
 
                 # Now clip the values to [0, 1] and plot
                 face_color = np.clip(face_color, 0, 1)
-                polygon = patches.Polygon(face, closed=True, facecolor=face_color[0], alpha=1)
+
+                print('face color value: ', (face_color[0], face_color[0] face_color[0]))
+                polygon = patches.Polygon(face, closed=True, facecolor=(face_color[0], face_color[0] face_color[0]), alpha=1)
                 ax.add_patch(polygon)    
 
         # Set plot limits and labels
