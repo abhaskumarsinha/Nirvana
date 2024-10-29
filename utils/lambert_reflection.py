a4da61871fdd74187c11801db6d4460622c7758c
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-29 16:30:10 +0530

    barycentric func import

--- utils/lambert_reflection.py
+++ utils/lambert_reflection.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from Nirvana.utils.barycentric_function import *
+
 def lambert_pipeline(vertices,
                      uv,
                      texture,
