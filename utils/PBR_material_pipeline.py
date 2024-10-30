a0f8ad4b491647ccbd0af00f82bbfcf8392fcb76
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-10-30 20:37:58 +0530

    Create PBR_material_pipeline.py

---
+++ utils/PBR_material_pipeline.py
@@ -0,0 +1,13 @@
+def PBR_pipeline_texture(canvas,
+                         face,
+                         texture,
+                         oa,
+                         normal_tex,
+                         roughness,
+                         gloss,
+                         uv,
+                         normal,
+                         view_direction,
+                         H,
+                         self.fresnel_value):
+    # write code here
