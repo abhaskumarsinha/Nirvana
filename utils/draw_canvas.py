0cfb2396abb8e190661c1695f18adc22dbb22cfa
Abhas Kumar Sinha <31654395+abhaskumarsinha@users.noreply.github.com>
2024-11-02 00:11:07 +0530

    Left right and upside down flip options

--- utils/draw_canvas.py
+++ utils/draw_canvas.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-def plot_pixel(canvas, x, y, color, x_scene=(-10, 10), y_scene=(-10, 10), flip=True):
+def plot_pixel(canvas, x, y, color, x_scene=(-10, 10), y_scene=(-10, 10), 
+               upside_down=True, left_right=False):
     """
     Plots a single point in a given canvas array of specified resolution,
-    with an option to flip the y-axis for upright orientation.
+    with options to flip the y-axis (upside-down) and/or the x-axis (left-right).
 
     Parameters:
         canvas (numpy.ndarray): The canvas array of shape (x_size, y_size, 3).
@@ -12,7 +13,8 @@ def plot_pixel(canvas, x, y, color, x_scene=(-10, 10), y_scene=(-10, 10), flip=T
         color (tuple): RGB color as a tuple of (R, G, B), each value 0-255.
         x_scene (tuple): The range of x in the scene, default (-10, 10).
         y_scene (tuple): The range of y in the scene, default (-10, 10).
-        flip (bool): If True, flip the y-axis for upright orientation. Default is True.
+        upside_down (bool): If True, flip the y-axis for upright orientation. Default is True.
+        left_right (bool): If True, flip the x-axis for mirror image. Default is False.
     """
     x_size, y_size, _ = canvas.shape
     
@@ -22,12 +24,15 @@ def plot_pixel(canvas, x, y, color, x_scene=(-10, 10), y_scene=(-10, 10), flip=T
     
     # Map float x, y coordinates to integer canvas indices
     i = int(round((x - x_scene[0]) * x_scale))
+    j = int(round((y - y_scene[0]) * y_scale))
     
-    # Flip y-axis if flip is True
-    if flip:
-        j = int(round((y_scene[1] - y) * y_scale))  # Flip by using (y_scene[1] - y)
-    else:
-        j = int(round((y - y_scene[0]) * y_scale))  # Normal mapping
+    # Apply left-right flip if specified
+    if left_right:
+        i = x_size - 1 - i
+    
+    # Apply upside-down flip if specified
+    if upside_down:
+        j = y_size - 1 - j
     
     # Ensure i, j are within bounds of the canvas
     if 0 <= i < x_size and 0 <= j < y_size:
